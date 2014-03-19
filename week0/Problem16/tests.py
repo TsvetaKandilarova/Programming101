30aa6c5446520074697698baaa4c40c604e69645
TsvetaKandilarova <ts.kandilarova@gmail.com>
2014-03-19 11:10:10 +0200

    Biggest difference between two numbers

---
+++ week0/Problem16/tests.py
@@ -0,0 +1,22 @@
+# IMPORTS
+from solution import biggest_difference
+import unittest
+
+# main
+class MyTestCase(unittest.TestCase):
+    def test_one(self):
+        self.assertEqual(-1, biggest_difference([1,2]))
+
+    def test_two(self):
+        self.assertEqual(-4, biggest_difference([1,2,3,4,5]))
+
+    def test_three(self):
+        self.assertEqual(-9, biggest_difference([-10, -9, -1]))
+
+    def test_four(self):
+        self.assertEqual(-99, biggest_difference(range(100)))
+
+
+# PROGRAM RUN
+if __name__ == '__main__':
+    unittest.main()
