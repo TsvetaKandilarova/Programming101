36eb336eee7d2ccaf67a20f8e7cc8c4b11546ae2
TsvetaKandilarova <ts.kandilarova@gmail.com>
2014-03-19 11:22:44 +0200

    Generate file with random integers

---
+++ week0/ProblemF3/solution.py
@@ -0,0 +1,20 @@
+import sys
+from random import randint
+
+def generate_numbers(filename, n):
+    file_ = open(filename, "w")
+    for i in range(0, n):
+        file_.write(str(randint(1, 1000)) + " ")
+    file_.close()
+
+def main():
+    args = sys.argv
+    if len(args) < 3:
+        print("no filename or number passed")
+    else:
+        filename = sys.argv[1]
+        n = int(sys.argv[2])
+        generate_numbers(filename, n)
+
+if __name__ == '__main__':
+    main()
