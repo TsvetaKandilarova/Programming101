f23f469311a9ed619149cbe38fe4bc5159e911d7
TsvetaKandilarova <ts.kandilarova@gmail.com>
2014-03-19 11:17:10 +0200

    Sort array of fractions

---
+++ week0/Problem30/tests.py
@@ -0,0 +1,18 @@
+from solution import sort_fractions
+import unittest
+
+
+# main
+class MyTestCase(unittest.TestCase):
+    def test_one(self):
+        self.assertEqual([(1, 2), (2, 3)], sort_fractions([(2, 3), (1, 2)]))
+
+    def test_two(self):
+        self.assertEqual([(1, 3), (1, 2), (2, 3)], sort_fractions([(2, 3), (1, 2), (1, 3)]))
+
+    def test_three(self):
+        self.assertEqual([(22, 78), (15, 32), (5, 6), (7, 8), (9, 6), (22, 7)], sort_fractions([(5, 6), (22, 78), (22, 7), (7, 8), (9, 6), (15, 32)]))
+
+# PROGRAM RUN
+if __name__ == '__main__':
+    unittest.main()
