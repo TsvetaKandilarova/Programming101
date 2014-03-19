32575bfb55f60ae293d2b5187bc390a3b19ab12e
TsvetaKandilarova <ts.kandilarova@gmail.com>
2014-03-19 11:21:51 +0200

    Cat multiple file

---
+++ week0/ProblemF2/solution.py
@@ -0,0 +1,23 @@
+import sys
+
+def cat(filename):
+    file_ = open(filename, "r")
+    content = file_.read()
+    file_.close()
+
+    return content
+
+def cat2(filenames):
+	text = map(cat, filenames)
+	return "\n\n".join(text)
+
+def main():
+	args = sys.argv
+	if len(args) < 2:
+		print("no filenames passed")
+	else:
+		filenames = sys.argv[1:]
+		print(cat2(filenames))
+
+if __name__ == '__main__':
+    main()
