8a86893c0fc14b5c181ea92499618fd02f77cfb2
TsvetaKandilarova <ts.kandilarova@gmail.com>
2014-03-19 11:25:46 +0200

    tests all the problems

---
+++ week0/test_all.py
@@ -0,0 +1,17 @@
+import os
+from subprocess import call
+
+
+def locate_and_run(pattern):
+    for root, dirs, files in os.walk('./'):
+        for file in files:
+            if file.startswith(pattern):
+                call(['py', root + '/' + file])
+
+
+def main():
+    locate_and_run('tests')
+
+
+if __name__ == '__main__':
+    main()
